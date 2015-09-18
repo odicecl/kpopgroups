f9d3007b4130502b20d19eb3c23e9e502c700fb2
Odice <im@odice.me>
2015-09-18 04:31:50 -0300

    Construyo ruta artistas
    - Creo ruta para el manejo de artistas
    - Instalo cheerio para traducir el texto html rescatado de los json en
    nodo

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// rutas propias
+
+router.get('/artist', function(req, res) {
+  var archivoBase = require('./jsons/kpop1900.json');
+  var codigoHTML = archivoBase.parse.text["*"];
+
+  // USAR CHEERIO PARA TRANSFORMAR EL TEXTO HTML EN NODO y LUEGO RESCATAR LOS ELEMENTOS LI
+
+
+
+
+  var elementosLI = objetoBody.getElementsByTagName('li');
+  var textoRendereado = " ";
+  for (var i = 0; i < elementosLI.length; i++) {
+    textoRendereado = elementosLI[i].nodeValue;
+  }
+
+  res.render('artist', {'lista': textoRendereado});
+});
+
 module.exports = router;

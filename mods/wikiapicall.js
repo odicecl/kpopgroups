42605a430301c0d3ea6040ae132a6718c53a71d5
odicecl <im@odice.me>
2015-09-25 02:18:35 -0300

    Separacion y Creacion de archivo BD
    - Separo archivo con funciones del archivo de ruteo para artist
    - Aplico ruta con POST para solicitar datos de la api y volcarlas a un
    archivo JSON
    - Dejo pendiente el limpiar el codigo parseado de manera más elegante

---
+++ mods/wikiapicall.js
@@ -0,0 +1,104 @@
+﻿var express = require('express');
+var request = require('sync-request');
+var cheerio = require('cheerio');
+var fs = require('fs');
+var exports = module.exports = {};
+
+/** Obtiene array con nombres de artistas obtenidos desde la wikipedia **/
+exports.fnObtenerArtistas = function (arrNombres) {
+	var arrUrls = fnFormatearURLWiki(arrNombres);
+	var arrWikiParse = fnObtenerWikiParse(arrUrls);
+	var arrArtistas = fnObtenerListaArtistas(arrWikiParse);
+	// Guardo archivo
+	fnGuardarArtistas(arrArtistas);
+}
+
+/** Prepara las URL para las peticiones a la API de wikipedia **/
+function fnFormatearURLWiki(arrNombres) {
+	var pre = 'https://en.wikipedia.org/w/api.php?action=parse&format=json&page=';
+	var post = '&prop=text&mobileformat=';
+	for (var i = 0; i < arrNombres.length; i++) {
+		arrNombres[i] = pre + arrNombres[i] + post;
+	}
+	return (arrNombres);
+}
+
+/** Obtiene arreglo con el parse de las páginas de la wikipedia consultadas **/
+function fnObtenerWikiParse(arrUrls) {
+	var arrWikiParse = [];
+
+	for (var i = 0; i < arrUrls.length; i++) {
+		var res = request('GET', arrUrls[i]);
+		var jp = JSON.parse(res.getBody());
+		arrWikiParse.push(jp.parse.text['*']);
+	}
+	return (arrWikiParse);
+}
+
+/** Obtiene lista de artistas como una array de texto **/
+function fnObtenerListaArtistas(arrWikiParse) {
+	var arrArtistas = [];
+	for (var i = 0; i < arrWikiParse.length; i++) {
+		var $ = cheerio.load(arrWikiParse[i]);
+		fnLimpiaParse($,i);
+		// Quito los elementos sobrantes para todas las consultas
+		$('h2').remove();
+		$('sup').remove();
+		$('ol').remove();
+		$('#toc').remove();
+		$('.reflist').remove();
+		$('.navbox').remove();
+
+		// Para la página de artistas obtengo todos los elementos LI (Algunos Li tienen dentro el nombre del grupo como anchor por eso los distingo)
+		if (i == 3) {
+			$('li').each(function () {
+				arrArtistas.push($(this).text().toUpperCase());
+			});
+		}
+		else {
+			// Para todos los demás grupos borro las tablas (cosa que en el de idols no se puede o pierdo todo) y los links dentro de los li (para no agregar referencias adicionales)
+			$('table').remove();
+			$('li').find('a').each(function () {
+				arrArtistas.push($(this).text().toUpperCase());
+			});
+		}
+	}
+	return (arrArtistas.sort());
+}
+/**** TRABAJANDO AQUI **********/
+function fnLimpiaParse($,intParse) {
+	// intParse corresponde al número de consulta y sirve de indice
+	// Array con tags a eliminar y seleccionar (# para IDS . Para clases)
+	// Limpieza y selección por defecto
+	arrLimpieza = {
+		indice:-1
+		,limpiar:["h2","sup","ol","#toc",".reflist",".navbox"]
+		,seleccionar:["li","a"]
+	};
+	// Limpieza y selección espec;ifica
+	arrLimpieza = {
+		indice:3
+		,seleccionar:["li"]};
+
+	// Ciclo de Limpieza
+	if (arrLimpieza.indice === undefined) {
+		for (var i = 0; i < array.length; i++) {
+			array[i]
+		}
+	}
+}
+function fnGuardarArtistas(arrArtistas) {
+	var arrObjJSON = [];
+	for (var i = 0; i < arrArtistas.length; i++) {
+		var objTmp = new Object();
+		objTmp.nombre=arrArtistas[i];
+		arrObjJSON.push(objTmp);
+	}
+	var objJSON =JSON.stringify({artistas:arrObjJSON});
+	var strRutaArchivo = "bd/artistas.json";
+	var strMensajeGuardado = "";
+	fs.writeFile(strRutaArchivo, objJSON, function (err) {
+	(err)?console.log(err):console.log("Se guardó el archivo");
+	});
+	return (objJSON);
+}
--- routes/artist.js
+++ routes/artist.js
@@ -1,17 +1,13 @@
 /** Importación de Módulos **/
 var express = require('express');
 var router = express.Router();
-var request = require('sync-request');
-var cheerio = require('cheerio');
+var wiki = require('../mods/wikiapicall.js');
 
 
 /**
  * Definición de rutas de artistas
- * @param  {String} '/'           dirección a rutear
- * @param  {function} function(req, res,          next función que maneja requerimientos y respuestas
- * @return {Array String}               devuelve codigo html a mostrar con artistas
- */
-router.get('/', function(req, res, next) {
+ **/
+router.post('/', function(req, res, next) {
   // Array con nombre de las páginas de la wikipedia a parsear
   var arrNombreArtWiki = [
     'List_of_South_Korean_idol_groups_(1990s)'
@@ -19,89 +15,11 @@ router.get('/', function(req, res, next) {
     ,'List_of_South_Korean_idol_groups_(2010s)'
     ,'List_of_individual_K-pop_artists'
   ];
-  // Array con los artistas recibidos del parseo de la wikipedia
-  var arrArtistas = fnObtenerArtistas(arrNombreArtWiki);
-  // Envío de Array a la vista
-  res.render('artist', {artistas:arrArtistas});
-
-  /***
-  * TSUNAMI DE FUNCIONES
-  ***/
-
-  /**
-   * Obtiene array con nombres de artistas obtenidos desde la wikipedia
-   * @param  {array String} arrNombres Array con colección de nombres de las páginas de la Wiki a consultar
-   * @return {array String}            Array con nombres de artistas
-   */
-  function fnObtenerArtistas(arrNombres) {
-    var arrUrls = fnFormatearURLWiki(arrNombres);
-    var arrWikiParse = fnObtenerWikiParse(arrUrls);
-    var arrArtistas = fnObtenerListaArtistas(arrWikiParse);
-    return (arrArtistas);
-    }
-    /**
-     * Prepara las URL para las peticiones a la API de wikipedia
-     * @param  {array String} arrNombres Array con el nombre de las páginas de wikipedia a consultar
-     * @return {array String}            Devuelve array con las url formateadas
-     */
-  function fnFormatearURLWiki(arrNombres) {
-      var pre = 'https://en.wikipedia.org/w/api.php?action=parse&format=json&page=';
-      var post= '&prop=text&mobileformat=';
-      for (var i = 0; i < arrNombres.length; i++) {
-        arrNombres[i]=pre+arrNombres[i]+post;
-      }
-      return(arrNombres);
-    }
-    /**
-     * Obtiene arreglo con el parse de las páginas de la wikipedia consultadas
-     * @param  {array String} arrUrls Array con las urls a consultar a wikipedia
-     * @return {array String}         Parse resultado de la consulta a wikipedia
-     */
-  function fnObtenerWikiParse(arrUrls) {
-    var arrWikiParse = [];
-
-    for (var i = 0; i < arrUrls.length; i++) {
-        var res = request('GET',arrUrls[i]);
-        var jp = JSON.parse(res.getBody());
-        arrWikiParse.push(jp.parse.text['*']);
-      }
-    return(arrWikiParse);
-  }
-  /**
-   * Obtiene lista de artistas como una array de texto
-   * @param  {String} strHTMLWiki String con todo el codigo html reunido
-   * @return {array String}             Array con los nombres de los artistas
-   */
-  function fnObtenerListaArtistas(arrWikiParse) {
-
-    var arrArtistas = [];
-    for (var i = 0; i < arrWikiParse.length; i++) {
-      var $ = cheerio.load(arrWikiParse[i]);
-
-      // Quito los elementos sobrantes
-      $('h2').remove();
-      $('sup').remove();
-      $('ol').remove();
-      $('#toc').remove();
-      $('.reflist').remove();
-      $('.navbox').remove();
-
-      // La pagina de artistas individuales (3) es distinta
-      if(i!==3){
-        $('table').remove();
-        $('li').find('a').each(function(){
-            arrArtistas.push($(this).text().toUpperCase());
-          });
-      }
-      else {
-
-        $('li').each(function(){
-            arrArtistas.push($(this).text().toUpperCase());
-          });
-      }
-    }
-    return (arrArtistas.sort());
-  }
+  // Obtengo artistas desde la Wiki
+  wiki.fnObtenerArtistas(arrNombreArtWiki);
 });
+/*router.get('/', function (req, res) {
 
+}
+*/
 module.exports = router;
